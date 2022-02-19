e57678671eb84a8634e4bd7aa53028e366278cb5
Kyle <kyle@Kyles-MacBook-Pro.local>
2022-02-18 23:40:33 -0500

    Profile Picture section for the Profile Card and doggy image sample

--- src/Profile.js
+++ src/Profile.js
@@ -1,9 +1,37 @@
-function ProfilePage(){
+import React from 'react'
+import './Profile.css'
+// import img1 from './images/doggy.jpg'
 
+function ProfilePage(){
     return(
-    <h1> Header test idoasjdiasodj as</h1>
+        // body is implicit
+        <div className = 'ProfileCard'>
+            <div className = 'ProfileHeader'> 
+                <div className = 'ProfilePicture'>
+                    <img src = "./images/doggy.jpg" alt ="you" />
+                </div>
+
+                <div className = 'ProfileGreeting'> 
+                    <h3> Welcome back, () </h3>
+
+                </div>
+
+
+            </div>
+            
+            <div className = 'ProfileFooter'>
+                <button id = "SettingsButton"> settings </button>
+
+            </div>
+
+
+        </div>
+
+
+
+
+    // <h5> Header test idoasjdiasodj as</h5>
     )
-    
 }
     
 export default ProfilePage
