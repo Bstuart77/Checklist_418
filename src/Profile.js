f323f207f34e1f941a3fdce2dc5380c8b0673606
Kyle <kyle@kyles-mbp.home>
2022-02-22 01:23:14 -0500

    Added buttons that will change the background

--- src/Profile.js
+++ src/Profile.js
@@ -3,6 +3,14 @@ import './Profile.css'
 // import img1 from './images/doggy.jpg'
 
 function ProfilePage(){
+
+    function profile1(){
+        document.body.style.background = "url('./images/mountain.jpg') no-repeat center";
+        document.body.style.backgroundSize = "cover";
+        console.log("Button clicked");
+    }
+    
+
     return(
         // <button id = "SettingsButton"> settings </button>
         // body is implicit
@@ -14,7 +22,7 @@ function ProfilePage(){
                     </div>
 
                     <div className = 'ProfileGreeting'> 
-                        Welcome back, ()
+                        Welcome back, asdsadada
                     </div>
 
 
@@ -23,20 +31,41 @@ function ProfilePage(){
                 <div className = 'ProfileFooter'>
                     <button className = "Checklist"> checklist </button>
                     <button className = "ChangeProfile"> profile picture </button>
-                    <button className = "ChangeBackground"> background </button>
+                    <button className = "ChangeBackground"> back ground </button>
                     <button className = "Logout"> logout </button>
 
                 </div>
-
-
             </div>
+
+            <div className = 'ProfileModal'>
+                <button onClick = {profile1}> 1 </button>
+                <button className = 'profilebutton1'> 2 </button>
+                <button className = 'profilebutton1'> 3 </button>
+                <button className = 'profilebuttonuploadpic'> Upload Profile </button>
+                
+            
+            </div>
+
+            <div className = 'BackgroundModal'>
+            
+            </div>
+
+            <div className = 'Settings'>
+                /** This is where profile and background modal will eventually go to */
+            
+            </div>
+
         </div>
-
-
-
-
-    // <h5> Header test idoasjdiasodj as</h5>
     )
+
+
+    let btn1 = document.querySelector('#profilebutton1');
+
+    btn1.addEventListener('click', () => {
+        document.body.style.backgroundImage = "../public/images/mountain2.jpg";
+
+    });
+
 }
     
 export default ProfilePage
