fc30e267fdcfa2b95ac2c7c43fbb0f0472854278
Izaaz Izzadeen <izaazizzadeen@hotmail.com>
2022-02-24 11:04:02 -0500

    daily view start

--- src/ChecklistPage.js
+++ src/ChecklistPage.js
@@ -1,7 +1,27 @@
+import {useState} from 'react'
+//import {Box, Button} from '@mui/material'
+
 function ChecklistPage() {
     return (
+        <div className='DailyViewClass' 
+        style=
+        {{
+            width: 500, height: 500
+        }}> 
+        <h1 className='DailyViewHeader'>Daily View</h1> 
+        <h2 className= 'ToDoToday'>Stuff to Do Today</h2>
+        <h2>Testing</h2>
+        <h2>Testing</h2>
+        <h2>Testing</h2>
+            
+        <button className='ChecklistToProfile'>Go To Profile</button>
+        <button className='MonthlyView'>Monthly View</button>
+        <button className='DailyView'>Daily View</button>
+           
+        </div>
 
-        <h1 className="h1tag">header</h1>
+        
+
     )
 }
 
