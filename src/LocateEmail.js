11ecba8e4ecf2f78c219047c2e0e88b431c658a5
ange1a-huang <athuang@albany.edu>
2022-02-26 17:42:45 -0500

    added page for locating a User's login email.

---
+++ src/LocateEmail.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+function LocateEmail(){
+
+
+
+
+<>
+  <span class="circle">
+    <h1>Locate Your Email</h1> form goes here{" "}
+  </span>
+</>;
+}
+
+export default LocateEmail;
