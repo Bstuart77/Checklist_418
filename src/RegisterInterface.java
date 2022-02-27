fcceb627780efd55869a703d9b58e5d589d08c77
Jimmy Brooks <jgbrooks@albany.edu>
2022-02-27 17:05:25 -0500

    Completed functions for setting attributes on sql database

--- src/RegisterInterface.java
+++ src/RegisterInterface.java
@@ -5,8 +5,8 @@ import java.sql.SQLException;
 public interface RegisterInterface{
     void dbConnection(String query) throws SQLException;
     int setEmailAttribute(String emailAddress) throws SQLException;
-    int setNameAttribute(String firstName, String lastName);
-    int setUserAttribute(String username);
+    int setFirstNameAttribute(String firstName);
+    int setLastNameAttribute(String lastName);
     int setPassAttribute(String password);
 
 }
