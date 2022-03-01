c617d6be04342e88d1f8021c6201ff68f995e162
Izaaz Izzadeen <izaazizzadeen@hotmail.com>
2022-03-01 18:07:25 -0500

    add a task page

---
+++ src/AddTaskPage.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import "./AddTaskPage.css";
+
+function AddTaskPage() {
+
+    function CategoryDropdown(){
+        return(
+            <div className = "CategoryDropdown">
+                
+            </div>
+        )
+    }
+
+    return (
+        <div className = 'AddTaskCard' 
+        style=
+        {{
+            width: 500, height: 500
+        }}> 
+        
+        <h1 className='AddTaskHeader'> Add a Task </h1> 
+        
+        <form>
+            <label className = 'TitleLabel'> Title </label>
+        <br/>
+            <input className = 'TitleBox' 
+                type = "text" required
+                placeholder="Ex: Do Homework"
+            />
+        <br/>
+        <br/>
+            <label className = 'DateTimeLabel'> Date and Time </label>
+        <br/>
+            <input className = 'DateTimeBox' 
+                type = "text" required
+                placeholder="Ex: March 15 by 3:00"
+            />
+        <br/>
+        <br/>   
+            <label className = 'CategoryLabel'> Category </label>
+            <label className = 'ColorLabel'> Color </label>
+        <br/>
+         
+
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+            <input className = 'NotesBox' 
+                type = "text" required
+                placeholder="Notes"
+            />
+    
+        </form>
+            
+        <button className='AttachFilesButton'> Attach Files </button>
+        <button className='CancelButton'> Cancel </button>
+        <button className='SaveButton'> Save </button>
+           
+        </div>
+    )
+}
+
+export default AddTaskPage
