dd771d64651b42e94081e23efc4e6ae1b02d75c4
Brian Stuart <bstuart777@gmail.com>
2022-03-24 15:36:48 -0400

    Removed google log in code

--- src/App.js
+++ src/App.js
@@ -4,22 +4,9 @@ import Profile from "./Profile";
 import Checklist from "./ChecklistPage";
 import ResetPassword from "./ResetPassword";
 import LocateEmail from "./LocateEmail";
-import {gapi} from 'gapi-script'
-import { useEffect } from "react";
-const clientId = "222186129129-lheckv1f7jfcq4225n35ob7aed05tooc.apps.googleusercontent.com"
+
 
 function App() {
-
-  useEffect(() =>{
-    function start(){
-      gapi.client.init({
-        clientId: clientId,
-        scope: ""
-      })
-    };
-    gapi.load('client:auth2',start);
-  })
-
   return (
     <div className="App">
   
@@ -30,8 +17,8 @@ function App() {
       {/* <LocateEmail/> */}
       
       {/* <SignUp />    
-      <img src="images/spiral.PNG" className='SignUpSpiral1' /><img src="images/spiral.PNG" className='SignUpSpiral2' /><img src="images/spiral.PNG" className='SignUpSpiral3' /><img src="images/spiral.PNG" className='SignUpSpiral4' /><img src="images/spiral.PNG" className='SignUpSpiral5' /><img src="images/spiral.PNG" className='SignUpSpiral6' /> */}
-      
+      <img src="images/spiral.PNG" className='SignUpSpiral1' /><img src="images/spiral.PNG" className='SignUpSpiral2' /><img src="images/spiral.PNG" className='SignUpSpiral3' /><img src="images/spiral.PNG" className='SignUpSpiral4' /><img src="images/spiral.PNG" className='SignUpSpiral5' /><img src="images/spiral.PNG" className='SignUpSpiral6' />
+       */}
 
       {/* <Profile/>  */}
     </div>
