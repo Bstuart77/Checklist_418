cc5b6aab6fb50e427468e18cee6a350133a2cab7
Brian Stuart <bstuart777@gmail.com>
2022-03-24 15:35:45 -0400

    Removed Google Log In Code

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import GoogleLogin, { GoogleLogIn } from 'react-google-login'
-const clientId = "222186129129-lheckv1f7jfcq4225n35ob7aed05tooc.apps.googleusercontent.com"
 
 function SignUp() {
 
@@ -31,32 +29,6 @@ function SignUp() {
             console.log(signUpInfo);
         }
     }
-
-    const refreshTokenSetup = (res) => {
-        // Timing to renew access token
-        let refreshTiming = (res.tokenObj.expires_in || 3600 - 5 * 60) * 1000;
-
-        const refreshToken = async () => {
-            const newAuthRes = await res.reloadAuthResponse();
-            refreshTiming = (newAuthRes.expires_in || 3600 - 5 * 60) * 1000;
-            console.log('newAuthRes:', newAuthRes);
-            // saveUserToken(newAuthRes.access_token);  <-- save new token
-            localStorage.setItem('authToken', newAuthRes.id_token);
-
-            // Setup the other timer after the first one
-            setTimeout(refreshToken, refreshTiming);
-        };
-
-        // Setup first refresh timer
-        setTimeout(refreshToken, refreshTiming);
-    };
-
-    const onSuccess = (res) => {
-        console.log("Logged IN AS: ", res.profileObj);
-        // console.log(res.user.givenName);
-        refreshTokenSetup(res);
-    }
-
     return (
         <div className="SignUpClass">
             <form>
@@ -98,10 +70,6 @@ function SignUp() {
 
                 <button className="SignUpButton" onClick={submitSignUp}>Sign Up</button>
             </form>
-            <br />
-
-
-            <GoogleLogin clientId={clientId} onSuccess={onSuccess} buttonText='Sign In With Google' theme='dark' isSignedIn={true} className='SignInWithGoogleButton' />
         </div>
     );
 }
