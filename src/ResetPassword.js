5af82be870bf68a7aa7100d6c0ac1f2cdf3fb7b6
ange1a-huang <athuang@albany.edu>
2022-03-28 21:54:36 -0400

    Banned bad inputs for Password/Email.

--- src/ResetPassword.js
+++ src/ResetPassword.js
@@ -1,17 +1,22 @@
 import React, { useState } from "react";
 import "./ResetPassword.css";
+// TODO upon successfully obtaining a new password from the user, send it to the backend for recording, get the okay status from backend, and then take User to a new page.
 
 function ResetPassword() {
   const [newPassword, setNewPassword] = useState("");
 
-  const submitSignUp = (e) => {
+  const submissionHandler = (e) => {
     e.preventDefault();
 
     const resetPassword = { newPassword };
-    if (!newPassword.trim()) {
+    if (newPassword.length == 1 || newPassword.length <= 7){
+      alert("Your password has to be at least 7 characters long.");
+      setNewPassword("");
+    } else if (!newPassword.trim()) {
       alert("Please enter a new password. Don't leave it blank.");
     } else {
       console.log(resetPassword);
+      // send info to backend for recording
     }
   };
 
@@ -33,7 +38,7 @@ function ResetPassword() {
             />
             <br />
             <br />
-            <button className="new-password-button" onClick={submitSignUp}>
+            <button className="new-password-button" onClick={submissionHandler}>
               Reset My Password
             </button>
           </form>
