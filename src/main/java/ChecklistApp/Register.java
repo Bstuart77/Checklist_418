113811f5dcdd6e82e76de4273ab2dc7aa70ecdd7
Jimmy Brooks <jgbrooks@albany.edu>
2022-03-28 20:42:13 -0400

    Fixed a bug where register was being mapped more than one time.

--- src/main/java/ChecklistApp/Register.java
+++ src/main/java/ChecklistApp/Register.java
@@ -7,101 +7,102 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.*;
-
 
 @RestController
-public class Register implements RegisterInterface  {
-    String url = "jdbc:postgresql://localhost:5432/ICSI418Y";
-    String dbUsername = "postgres";
-    String password = "database1";
+public class Register {
     int userID;
 
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    /**
-     * Function to establish a connection between the JDBC driver and the Postgresql Database.
-     * @param query
-     * @throws SQLException
-     */
-    public void dbConnection(String query) throws SQLException{
-        Connection con = DriverManager.getConnection(url,dbUsername,password);
-        Statement statement = con.createStatement();
-        ResultSet resultFromDB = statement.executeQuery(query);
+
+
+    @RequestMapping(method = RequestMethod.POST,path = "/register")
+    public void register(@RequestParam("emailAddress")String emailAddress,
+                         @RequestParam("firstName") String firstName,
+                         @RequestParam("lastName") String lastName,
+                         @RequestParam("password") String password){
+        jdbcTemplate.execute("UPDATE usersDatabase SET userEmail = " + emailAddress + "WHERE userID =" + userID);
+        jdbcTemplate.execute("UPDATE usersDatabase SET userFirstName = " + firstName + "WHERE userID =" + userID);
+        jdbcTemplate.execute("UPDATE usersDatabase SET userLastName = " + lastName + "WHERE userID =" + userID);
+        jdbcTemplate.execute("UPDATE usersDatabase SET userPass = " + password + "WHERE userID =" + userID);
+
+
+
+
     }
 
-    /**
-     * Function to set an email address attribute in the sql database.
-     * @param emailAddress
-     * @return
-     * @throws SQLException
-     */
-    @RequestMapping(method = RequestMethod.POST, value = "/register")
-    public int setEmailAttribute(@RequestParam("emailAddress")String emailAddress) throws SQLException {
-        try {
-            jdbcTemplate.execute("UPDATE usersDatabase SET userEmail = " + emailAddress + "WHERE userID =" + userID);
-        } catch(Exception e){
-            return 500;
-        }
-        return 200;
-        // set the email attribute in the database. //Complete
-        // if successful return code 200.//Complete
-        // else return server error.//Complete
-    }
-
-    /**
-     * Function to set the first name attribute in the sql database.
-     * @param firstname
-     * @return
-     */
-    @RequestMapping(method = RequestMethod.POST, value = "/register")
-    public int setFirstNameAttribute(@RequestParam("firstName")String firstname){
-        try {
-            jdbcTemplate.execute("UPDATE usersDatabase SET userFirstName = " + firstname + "WHERE userID =" + userID);
-        } catch(Exception e){
-            return 500;
-        }
-        return 200;
-        // set the user firstname in the database. //Complete
-        // if successful return code 200.//Complete
-        // else return server error.//Complete
-    }
-
-    /**
-     * Function to set the last name attribute in the sql database.
-     * @param lastname
-     * @return
-     */
-    @RequestMapping(method = RequestMethod.POST, value = "/register")
-    public int setLastNameAttribute(@RequestParam("lastName")String lastname) {
-        try {
-            jdbcTemplate.execute("UPDATE usersDatabase SET userLastName = " + lastname + "WHERE userID =" + userID);
-        } catch(Exception e){
-            return 500;
-        }
-        return 200;
-        // set the user last name in the database. //Complete
-        // if successful return code 200.//Complete
-        // else return server error.//Complete
-    }
-
-    /**
-     * Function to set the password attribute in the sql database.
-     * @param password
-     * @return
-     */
-    @RequestMapping(method = RequestMethod.POST, value = "/register")
-    public int setPassAttribute(@RequestParam("password") String password){
-        try {
-            jdbcTemplate.execute("UPDATE usersDatabase SET userPass = " + password + "WHERE userID =" + userID);
-        } catch(Exception e){
-            return 500;
-        }
-        return 200;
-        // set the password with the correct data entry.
-        // if successful return code 200
-        // else return server error.
-    }
+//    /**
+//     * Function to set an email address attribute in the sql database.
+//     * @param emailAddress
+//     * @return
+//     * @throws SQLException
+//     */
+//    @RequestMapping(method = RequestMethod.POST, path = "/register")
+//    public int setEmailAttribute(@RequestParam("emailAddress")String emailAddress) throws SQLException {
+//        try {
+//
+//        } catch(Exception e){
+//            return 500;
+//        }
+//        return 200;
+//        // set the email attribute in the database. //Complete
+//        // if successful return code 200.//Complete
+//        // else return server error.//Complete
+//    }
+//
+//    /**
+//     * Function to set the first name attribute in the sql database.
+//     * @param firstname
+//     * @return
+//     */
+//    @RequestMapping(method = RequestMethod.POST, path = "/register")
+//    public int setFirstNameAttribute(@RequestParam("firstName")String firstname){
+//        try {
+//
+//        } catch(Exception e){
+//            return 500;
+//        }
+//        return 200;
+//        // set the user firstname in the database. //Complete
+//        // if successful return code 200.//Complete
+//        // else return server error.//Complete
+//    }
+//
+//    /**
+//     * Function to set the last name attribute in the sql database.
+//     * @param lastname
+//     * @return
+//     */
+//    @RequestMapping(method = RequestMethod.POST, path = "/register")
+//    public int setLastNameAttribute(@RequestParam("lastName")String lastname) {
+//        try {
+//
+//        } catch(Exception e){
+//            return 500;
+//        }
+//        return 200;
+//        // set the user last name in the database. //Complete
+//        // if successful return code 200.//Complete
+//        // else return server error.//Complete
+//    }
+//
+//    /**
+//     * Function to set the password attribute in the sql database.
+//     * @param password
+//     * @return
+//     */
+//    @RequestMapping(method = RequestMethod.POST, path = "/register")
+//    public int setPassAttribute(@RequestParam("password") String password){
+//        try {
+//
+//        } catch(Exception e){
+//            return 500;
+//        }
+//        return 200;
+//        // set the password with the correct data entry.
+//        // if successful return code 200
+//        // else return server error.
+//    }
 
 }
