dffbe4cd8cbd09b16de478bedc3a27f2b7a3299c
samnisbet <sammienisbet@gmail.com>
2022-04-07 17:05:25 -0400

    Add Task and Task Repository

--- src/main/java/ChecklistApp/Task.java
+++ src/main/java/ChecklistApp/Task.java
@@ -1,5 +1,50 @@
 package ChecklistApp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Task {
+
+    private String title;
+    private String description;
+
+    private Date startDate;
+    private Date endDate;
+
+    public String getTitle(){ 
+        return this.title;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public Date getStartDate(){
+        return this.startDate;
+    }
+
+    public Date getEndDate(){
+        return this.endDate;
+    }
+
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public void setStartDate(String startDate) throws ParseException{
+        this.startDate = new SimpleDateFormat("MM/dd/yy").parse(startDate);
+    }
+
+    public void setEndDate(String endDate) throws ParseException{
+        this.endDate = new SimpleDateFormat("MM/dd/yy").parse(endDate);
+    }
 
 }
