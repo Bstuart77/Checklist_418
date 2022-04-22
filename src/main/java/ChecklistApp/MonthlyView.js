18679af207ef0d6a7c437f6ffe5f4c48f66f4005
Patrizzia Espina <pespina@albany.edu>
2022-04-22 11:31:49 -0400

    Added completed task/point adder

--- src/main/java/ChecklistApp/MonthlyView.js
+++ src/main/java/ChecklistApp/MonthlyView.js
@@ -2,10 +2,10 @@ import format from "date-fns/format";
 import getDay from "date-fns/getDay";
 import parse from "date-fns/parse";
 import startOfWeek from "date-fns/startOfWeek";
-import React, { useState } from "react";
-import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import React, { useState, useCallback, useMemo, Fragment } from "react";
+import { Calendar, dateFnsLocalizer, Views } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-
+import PropTypes from 'prop-types'
 
 const locales = {
   "en-US": require("date-fns/locale/en-US"),
@@ -20,100 +20,79 @@ const localizer = dateFnsLocalizer({
 });
 
 const events = [
-  {
-      title: "409 exam",
-      start: new Date(2022, 2, 28),
-      end: new Date(2022, 2, 29),
-  },
-  {
-      title: "Capstone Milestone due",
-      start: new Date(2022, 2, 30),
-      end: new Date(2022, 2, 31),
-  },
-  {
-    title: "Sprint 3 due",
-    start: new Date(2022, 2, 29),
-    end: new Date(2022, 2, 30),
-},
+
 ];
 
 function DisplayNewEventModal(){
-    if(document.getElementById("NewEventModalUpdate").style.display === "block"){
-        document.getElementById("NewEventModalUpdate").style.display = "none";
-    } else {
-        document.getElementById("NewEventModalUpdate").style.display = "block";
-    }
+  if(document.getElementById("NewEventModalUpdate").style.display === "block"){
+      document.getElementById("NewEventModalUpdate").style.display = "none";
+  } else {
+      document.getElementById("NewEventModalUpdate").style.display = "block";
+  }
 
 }
 
 function DisplayDeleteEventModal(){
-  
+
 }
 
 window.onclick = function (event) {
-  if (event.target == document.getElementById("NewEventModalUpdate")) {
-      document.getElementById("NewEventModalUpdate").style.display = "none";
-  }
+if (event.target == document.getElementById("NewEventModalUpdate")) {
+    document.getElementById("NewEventModalUpdate").style.display = "none";
+}
 }
 
 
 function MonthlyView() {
-  const [newEvent, setNewEvent] = useState({title: "", start: "", end: ""})
-    const [allEvents, setAllEvents] = useState(events)
+const [newEvent, setNewEvent] = useState({title: "", start: "", end: ""})
+const [allEvents, setAllEvents] = useState(events)
 
-    function handleAddEvent() {
-      setAllEvents([...allEvents, newEvent])
-    }
-    return (
-      <div className="MonthlyView">
-        <h1>Calendar</h1>
+  function handleAddEvent() {
+    setAllEvents([...allEvents, newEvent])
+  }
 
-        <button className="MonthlyViewButton" onClick={DisplayNewEventModal}> Create New Event </button>
+  function handleAddPoint(){
+    console.log("Added a point");
+    pointCount = pointCount + 1;
+    
+  }
+  return (
+    <div className="MonthlyView">
+       <h1 className="CalendarTitle">Calendar</h1>
 
-        <div className = 'NewEventModal' id = "NewEventModalUpdate">
-            <div className = 'NewEventModalBody'>
-              <h2>Add New Event</h2>
+      <button className="CreateEventButton" onClick={DisplayNewEventModal}> Create New Event </button>
 
-              <div>
-                <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
-                <input type="text" placeholder="start" style={{ width: "20%", marginRight: "10px" }} value={newEvent.start} onChange={(e) => setNewEvent({ ...newEvent, start: e.target.value })} />
-                <input type="text" placeholder="end" style={{ width: "20%", marginRight: "10px" }} value={newEvent.end} onChange={(e) => setNewEvent({ ...newEvent, end: e.target.value })} />
-                
-                <button style={{marginTop: "10px"}} onClick = {handleAddEvent}>
-                  Add Event
-                </button>
-              </div>
-
-
-
+      <div className = 'NewEventModal' id = "NewEventModalUpdate">
+          <div className = 'NewEventModalBody'>
+            <h2>Add New Event</h2>
+            <div>
+              <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
+              <input type="text" placeholder="start" style={{ width: "20%", marginRight: "10px" }} value={newEvent.start} onChange={(e) => setNewEvent({ ...newEvent, start: e.target.value })} />
+              <input type="text" placeholder="end" style={{ width: "20%", marginRight: "10px" }} value={newEvent.end} onChange={(e) => setNewEvent({ ...newEvent, end: e.target.value })} />
+              
+              <button style={{marginTop: "10px"}} onClick = {handleAddEvent}>
+                Add Event
+              </button>
             </div>
-        </div>
-
-        {/*
-          A work in progress for the DeleteEventModal
-        */}
-        <div className = 'DeleteEventModal'>
-          <h2>Delete Event</h2>
-          <div>
-            <input type="text" placeholder="Add Title" style={{ width: "20%", marginRight: "10px" }} value={newEvent.title} onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
-            <input type="text" placeholder="start" style={{ width: "20%", marginRight: "10px" }} value={newEvent.start} onChange={(e) => setNewEvent({ ...newEvent, start: e.target.value })} />
-            <input type="text" placeholder="end" style={{ width: "20%", marginRight: "10px" }} value={newEvent.end} onChange={(e) => setNewEvent({ ...newEvent, end: e.target.value })} />
-            
-            <button style={{marginTop: "10px"}} onClick = {handleAddEvent}>
-              Add Event
-            </button>
           </div>
-
-        </div>
-
-       <Calendar className="calendar" 
-          localizer = {localizer} 
-          events = {allEvents} 
-          startAccessor = "start" 
-          endAccessor = "end" 
-          style = {{ height: 500, margin: "50px" }}/>
       </div>
-  );
+
+     <Calendar className="calendar" 
+        localizer = {localizer} 
+        events = {allEvents} 
+        startAccessor = "start" 
+        endAccessor = "end" 
+        style = {{ height: 500, margin: "50px" }}/>
+
+    <div className='CompleteTask'>
+      <h3>Did you complete a task today?</h3>
+      <button style={{marginTop: "10px"}} onClick = {handleAddPoint}>
+        Yes
+      </button>
+    </div>
+
+    </div>
+);
 }
 
 export default MonthlyView;
